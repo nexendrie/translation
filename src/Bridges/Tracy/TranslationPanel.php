a8bfa3c8b32372567265fe915c502468230ab771
Jakub Konečný <jakub.konecny2@centrum.cz>
2019-01-18 16:30:21 +0100

    updated interface name in TranslationPanel

--- src/Bridges/Tracy/TranslationPanel.php
+++ src/Bridges/Tracy/TranslationPanel.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Nexendrie\Translation\Bridges\Tracy;
 
-use Nexendrie\Translation\Loaders\ILoader;
+use Nexendrie\Translation\ILoader;
 use Nexendrie\Translation\Translator;
 use Tracy\IBarPanel;
 
